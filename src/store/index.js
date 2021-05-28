053090604b7b19917311f0092381078110946a69
Oleg <chibikstrong@gmail.com>
2021-05-28 22:17:40 +0300

    local storage added.

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer'
 import { createLogger} from 'redux-logger'
 
+const persistedState = localStorage.getItem('state') ? JSON.parse(localStorage.getItem('state')) : []
 
-export const store = createStore(rootReducer, 
+export const store = createStore(rootReducer, persistedState,
                     applyMiddleware(createLogger()))
+
+store.subscribe(() => {
+    localStorage.setItem('state', JSON.stringify(store.getState()))
+})
